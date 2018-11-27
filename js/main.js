fbb100936e435a60bbafc1a84823208928dde01d
vertigoner <nbroberts97@gmail.com>
2018-11-27 00:52:43 -0500

    compile data into map

--- js/main.js
+++ js/main.js
@@ -28,23 +28,66 @@ function start() {
                     .attr("width",width * 2)
                     .attr("height",height);
 
-    var bubble = d3.layout.pack()
-        .size([diameter, diameter])
-        .padding(3)
-        .value(function(d) {
-            return d.size;
-        })
+    // var bubble = d3.layout.pack()
+    //     .size([diameter, diameter])
+    //     .padding(3)
+    //     .value(function(d) {
+    //         return d.size;
+    //     })
 
-    d3.csv("./data/candy.csv", function(csv) {
+    d3.csv("./data/candy.csv", function(error, data) {
+        if (error) {
+            console.error("Error getting or parsing the data.");
+            throw error;
+        }
 
-        var nodes = bubble.nodes(csv);
-        chart1.selectAll('circle')
-            .data(nodes)
-            .enter()
-            .append('circle')
-            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
-            .attr('r', function(d) { return d.r; })
-            .attr('class', function(d) { return d.className; });
+        var candyMap = d3.map();
+
+        for (let col of Object.keys(data[0])) {
+            if (/^Q6_/.test(col)) {
+                candyMap.set(col.substring(3), 0);
+            }
+        }
+
+        data.forEach(function(d) {
+            for (let col of Object.keys(d)) {
+                if (/^Q6_/.test(col)) {
+                    let key = col.substring(3);
+                    let val = candyMap.get(key);
+
+                    if (d[col] === "JOY") {
+                        val += 2;
+                    } else if (d[col] === "MEH") {
+                        val += 1;
+                    }
+
+                    candyMap.set(key, val);
+                }
+            }
+        });
+        console.log(candyMap);
+        
+        // chart1.selectAll('circle')
+        //     .data(data)
+        //     .enter()
+        //     .append('circle')
+        //     .attr()
+
+
+        // var scaleRadius = d3.scaleLinear()
+        //     .domain([d3.min(data, function(d) { return +d.views; }), 
+        //             d3.max(data, function(d) { return +d.views; })])
+        //     .range([5,18]);
+
+        // var bubbleChart = bubbleChart();
+        // chart1.data(data).call(bubbleChart);
     });
 }
+
+// function bubbleChart() {
+//     function chart(selecttion) {
+
+//     }
+//     return chart;
+// }
 
