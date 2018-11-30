5d8a93d635551bfe7b53e044131de6da5d7eb041
vertigoner <nbroberts97@gmail.com>
2018-11-30 01:31:05 -0500

    tooltip for bar chart

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ var colorRamp = ['#5D2B7D','#A72D89','#1474BB','#8FC33E','#FEEE22','#E41E26'];
 var neutralColor = "lightgray";
 var selectedColors = ["red", "blue", "green", "orange", "yellow"];
 var maxSelected = 5;
+var chart2BottomPadding = 50;
+var chart2LeftPadding = 100;
+var chart2TopPadding = 50;
 
 var selected = [];
 
@@ -52,11 +55,11 @@ function start() {
         .range([0, colorRamp.length])
     
     var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], 0.5);
+        .rangeRoundBands([0, width - chart2LeftPadding], 0.3);
     
     var y = d3.scale.linear()
         .domain([1, 2600])
-        .range([0, height]);
+        .range([height - chart2BottomPadding - chart2TopPadding, 0]);
 
     var candyMap = d3.map();
 
@@ -116,7 +119,7 @@ function start() {
                 return colorRamp[Math.floor(colorScale(d.value.joy * 2 + d.value.meh))];
             });
 
-        g.append("svg:text")
+        g.append("text")
             .text(function(d) { return d.key; });
         
         force.on("tick", tick);
@@ -138,8 +141,42 @@ function start() {
                 });
         }
 
+        // create chart2
+
+        chart2.append("g")
+            .attr("transform", "translate(" + chart2LeftPadding + "," + (height - chart2BottomPadding) + ")")
+            .attr("class", "axis")
+            .call(d3.svg.axis().scale(x));
+
+        chart2.append("text")             
+            .attr("transform", "translate(" + ((width - chart2LeftPadding) / 2 + chart2LeftPadding) + "," + (height - chart2BottomPadding / 2) + ")")
+            .style("text-anchor", "middle")
+            .text("Selected Candy");
+    
+
+        chart2.append("g")
+            .attr("transform", "translate(" + chart2LeftPadding + "," + chart2TopPadding + ")")
+            .attr("class", "axis")
+            .call(d3.svg.axis().scale(y).orient("left"));
+
+        chart2.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 15)
+            .attr("x", 0 - height / 2)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Rating");
+
+        var tooltip = d3.select("body").append("div")	
+            .attr("class", "tooltip card")				
+            .style("opacity", 0)
+        tooltip.append("span").attr("class", "name");
+        tooltip.append("span").attr("class", "joy");
+        tooltip.append("span").attr("class", "meh");
+        tooltip.append("span").attr("class", "despair");
+
         chart1.on("click", function() {
-            if (this === d3.event.target) {
+            if (this === d3.event.target || selected.length === maxSelected) {
                 chart1.selectAll("circle").attr("fill", function(d) {
                     return colorRamp[Math.floor(colorScale(d.value.joy * 2 + d.value.meh))];
                 });
@@ -159,29 +196,40 @@ function start() {
                 }
             }
 
+            chart2.selectAll("rect").data([]).exit().remove();
+
             // map ordinal data to x axis
             x.domain(selected.map(function(d) {
                 return d.key;
             }));
-
-            chart2.selectAll("rect").data([]).exit().remove();
             
             chart2.selectAll("rect")
                 .data(selected).enter()
                 .append("rect")
                 .attr("x", function(d) {
-                    console.log(d.key);
-                    return x(d.key);
+                    return x(d.key) + chart2LeftPadding;
                 })
                 .attr("width", x.rangeBand())
                 .attr("y", function(d) {
-                    return y(d.value.joy)
+                    return y(2 * d.value.joy + d.value.meh);
                 })
                 .attr("height", function(d) {
-                    return height - y(d.value.joy);
+                    return height - chart2BottomPadding - y(2 * d.value.joy + d.value.meh);
                 })
                 .attr("fill", function(d) {
                     return d.fill;
+                })
+                .on("mousemove", function(d) {
+                    tooltip.style("opacity", 0.9)
+                        .style("left", d3.event.pageX + "px")
+                        .style("top", d3.event.pageY + "px")
+                    tooltip.select(".name").text(d.key);
+                    tooltip.select(".joy").text("'Joy' votes: " + d.value.joy);
+                    tooltip.select(".meh").text("'Meh' votes: " + d.value.meh);
+                    tooltip.select(".despair").text("'Despair' votes: " + d.value.despair);
+                })
+                .on("mouseout", function(d) {
+                    tooltip.style("opacity", 0);
                 });
             
 
